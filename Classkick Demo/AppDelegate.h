d8e538342252b0727f2f3e288c1ecb2185e6bddf
Zmcclatc <mcclatch71@hotmail.com>
2015-06-08 07:13:26 -0700

    Initial Commit

---
+++ Classkick Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Classkick Demo
+//
+//  Created by Zoe M on 6/8/15.
+//  Copyright (c) 2015 Classkick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Classkick Demo/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Classkick Demo
+//
+//  Created by Zoe M on 6/8/15.
+//  Copyright (c) 2015 Classkick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
